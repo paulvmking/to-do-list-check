32e6aa248dd579143947dd41b60e61b792350d85
paulvmking <paulvmking@gmail.com>
2021-03-22 20:40:49 +0000

    added start of js to select inputs and buttons

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+	var tasker = {
+		init: function() {
+			this.cacheDom();
+			this.bindEvents();
+			this.evalTasklist();
+		},
+		cacheDom: function() {
+			this.taskInput = document.getElementById("input-task");
+			this.addBtn = document.getElementById("add-task-btn");
+			this.tasklist = document.getElementById("tasks");
+			this.tasklistChildren = this.tasklist.children;
+			this.errorMessage = document.getElementById("error");
+        }
+    }}) 
